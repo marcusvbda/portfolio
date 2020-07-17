db4c35f75f27d82398825e71dfd320b0c37323cf
Vinicius Bassalobre <=>
2020-07-17 20:59:58 -0300

    skils card

--- langs/EN.js
+++ langs/EN.js
@@ -1,6 +1,6 @@
 const EN = {
     "index.title": "Backend, Frontend Developer & Systems Analyst.",
-    "index.description": "I create and code wonderfully simple and functional things and I love what I do.",
+    "index.description": "I create and code incredibly simple and functional things and I love what I do.",
     "index.presentation": "Hi, I’m Marcus Vinicius. Nice to meet you.",
     "index.presentation_description": "Since I started my journey working as a backend and frontend developer almost 10 years ago, I worked remotely and in person for agencies in São Paulo and Santa Catarina (both in Brazil). I participated and helped to form great teams to create digital products for commercial use. I am quietly confident, naturally curious and perpetually working to improve my skills, growing everyday.",
 
--- pages/index.js
+++ pages/index.js
@@ -7,35 +7,55 @@ const Home = () => {
 
     const HomeSection = () => {
         return (
-            <div className="container">
-                <section id="home">
-                    <div className="body-hero flex-column x-center">
-                        <h1 className="mb-0 text-center f-30">{translate('index.title')}</h1>
-                        <h2 className="mb-0 font-light text-gray f-20 text-center">{translate('index.description')}</h2>
-                        <div className="flex-column mt-50 x-center y-center">
-                            <div className="avatar">
-                                <img className="w-100" src="/images/photo.png" />
-                            </div>
-                            <div className="hero-endsection">
-                                <img className="w-100" src="/images/hero.png" />
-                            </div>
+            <div className="container" id="home">
+                <div className="body-hero flex-column x-center">
+                    <h1 className="mb-0 text-center f-30">{translate('index.title')}</h1>
+                    <h2 className="mb-0 font-light text-gray f-20 text-center">{translate('index.description')}</h2>
+                    <div className="flex-column mt-50 x-center y-center">
+                        <div className="avatar">
+                            <img className="w-100" src="/images/photo.png" alt="avatar" />
+                        </div>
+                        <div className="hero-endsection">
+                            <img className="w-100" src="/images/hero.png" alt="hero" />
                         </div>
                     </div>
-                </section>
+                </div>
             </div>
         )
     }
 
     const AboutSection = () => {
         return (
-            <div className="about-hero flex-column x-center x-center y-center">
+            <div className="about-hero flex-column x-center x-center y-center" id="about">
                 <div className="container">
-                    <section id="about">
-                        <h4 className="presentation">{translate('index.presentation')}</h4>
+                    <h4 className="presentation">{translate('index.presentation')}</h4>
+                    <div className="flex-row x-center">
+                        <div className="presentation-description">{translate('index.presentation_description')}</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const SkillsSection = () => {
+        return (
+            <div className="skills-hero flex-column x-center x-center y-center" id="skills">
+                <div className="skills-cards">
+                    <div className="skills-card">
                         <div className="flex-row x-center">
-                            <div className="presentation-description">{translate('index.presentation_description')}</div>
+                            <img className="skill-icon" src="/images/frontend.png" />
                         </div>
-                    </section>
+                    </div>
+                    <div className="skills-card middle">
+                        <div className="flex-row x-center">
+                            <img className="skill-icon" src="/images/code.png" />
+                        </div>
+                    </div>
+                    <div className="skills-card">
+                        <div className="flex-row x-center">
+                            <img className="skill-icon" src="/images/mentor.png" />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
@@ -45,6 +65,7 @@ const Home = () => {
         <Template title="Home" image={systemConfig.icon}>
             {HomeSection()}
             {AboutSection()}
+            {SkillsSection()}
         </Template >
     )
 }
