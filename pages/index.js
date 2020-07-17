98bd223af6f01a387b7b4e194d707a8c6ec561a5
Vinicius Bassalobre <=>
2020-07-17 19:32:24 -0300

    about me

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,47 @@ import Template from '../components/template'
 const Home = () => {
     const { systemConfig, translate } = useContext(GlobalContext)
 
-    return (
-        <Template title="Home" image={systemConfig.icon}>
+    const HomeSection = () => {
+        return (
             <div className="container">
-                <div className="body-hero flex-column x-center">
-                    <h1 className="mb-0 text-center f-50">{translate('index.title')}</h1>
-                    <h2 className="mb-0 font-light text-gray f-20 text-center">{translate('index.description')}</h2>
-                    <div className="flex-row x-center mt-50">
-                        <div className="avatar">
-                            <img src="/images/photo.png" />
+                <section id="home">
+                    <div className="body-hero flex-column x-center">
+                        <h1 className="mb-0 text-center f-30">{translate('index.title')}</h1>
+                        <h2 className="mb-0 font-light text-gray f-20 text-center">{translate('index.description')}</h2>
+                        <div className="flex-column mt-50 x-center y-center">
+                            <div className="avatar">
+                                <img className="w-100" src="/images/photo.png" />
+                            </div>
+                            <div className="hero-endsection">
+                                <img className="w-100" src="/images/hero.png" />
+                            </div>
                         </div>
                     </div>
+                </section>
+            </div>
+        )
+    }
+
+    const AboutSection = () => {
+        return (
+            <div className="about-hero flex-column x-center x-center y-center">
+                <div className="container">
+                    <section id="about">
+                        <h4 className="presentation">{translate('index.presentation')}</h4>
+                        <div className="flex-row x-center">
+                            <div className="presentation-description">{translate('index.presentation_description')}</div>
+                        </div>
+                    </section>
                 </div>
             </div>
-        </Template>
+        )
+    }
+
+    return (
+        <Template title="Home" image={systemConfig.icon}>
+            {HomeSection()}
+            {AboutSection()}
+        </Template >
     )
 }
 export default Home
