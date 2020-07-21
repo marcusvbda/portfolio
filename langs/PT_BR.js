d6c8be863c52268ba533ca4aaea33f514734cf5f
Vinicius Bassalobre <=>
2020-07-21 16:55:03 -0300

    corrige texto title

--- langs/PT_BR.js
+++ langs/PT_BR.js
@@ -1,5 +1,5 @@
 const PT_BR = {
-    "index.title": "Desenvolvedor Dackend, Frontend & Analista de Sistemas.",
+    "index.title": "Desenvolvedor Backend, Frontend & Analista de Sistemas.",
     "index.description": "Crio e codifico coisas incrivelmente simples e funcionais e adoro o que faço.",
     "index.presentation": "Oi, Eu sou o Marcus Vinicius. Muito prazer.",
     "index.presentation_description": "Desde que comecei minha jornada como desenvolvedor backend e frontend há quase 10 anos, trabalhei remotamente e presencialmente para agências do interior e capital de São Paulo e Santa Catarina, participei e ajudei a formar grandes equipes para criar produtos digitais para uso comercial. Sou silenciosamente confiante, naturalmente curioso e perpetuamente trabalhando para melhorar minhas habilidades, crescendo a cada dia.",
