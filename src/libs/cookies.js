f13e639cd5dedd0e2718881526481bfc846f9807
Vinicius Bassalobre <=>
2021-03-31 18:29:29 -0300

    react resume

--- src/libs/cookies.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-import cookie from 'vue-cookies'
-Vue.prototype.$cookies = cookie
--- src/libs/progress.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import VueProgressBar from 'vue-progressbar'
-
-Vue.use(VueProgressBar, {
-    color: '#6A0E7F',
-    failedColor: '#dd0000',
-    thickness: '2px'
-})
